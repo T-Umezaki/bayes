543833e424f86c7f1160a4ed08e0bf938df62384
Takeshi_Umezaki <Takeshi_Umezaki@sis2.saison.co.jp>
2017-11-09 18:27:06 +0900

    整理

--- tegaki_req.py
+++ tegaki_req.py
@@ -31,24 +31,8 @@ def post_form(template_json_file, form_image_file):
 
     # Inject the base64-encoded form image into the template json
     template_json_data['imageData'] = encode_image(form_image_file)
-    # template_json_data['imageData'] = "aaa"
 
     # Send POST request to Tegaki service
-    ret = requests.get('http://www.yahoo.co.jp', proxies=PROXIES)
-    status = ret.status_code
-    print(status)
-
-    '''
-    try:
-        json_str = json.dumps(template_json_data, ensure_ascii=False)
-    except json.JSONDecodeError as e:
-        print('JSONDecodeError: ', e)
-    except Exception as e2:
-        print('Exception: ', e2)
-    print(len(json_str))
-    print("nyaa")
-    '''
-
     response = requests.post(TEGAKI_FORM_ENDPOINT,
                              headers={'Authorization': 'apikey ' + MY_API_KEY},
                              json=template_json_data, proxies=PROXIES)
@@ -59,33 +43,12 @@ def post_form(template_json_file, form_image_file):
 
 
 if __name__ == '__main__':
-    print("nya")
+    print("start")
     json_str = ""
-    '''
-    try:
-        # ローカルJSONファイルの読み込み
-        with open('sisco_format_fax.json', 'r', encoding="utf-8") as json_file:
-            json_dict = json.load(json_file)
-            print(len(json_dict))
-            json_str = json.dumps(json_dict, ensure_ascii=False)
-            print(len(json_str))
-    except json.JSONDecodeError as e:
-        print('JSONDecodeError: ', e)
-    '''
-    json_file = open("sisco_format_fax.json", "r", encoding="utf-8")
-    json_str = str(json_file.read())
-    json_file.close()
+    with open('sisco_format_fax.json', 'r', encoding="utf-8") as json_file:
+        json_str = str(json_file.read())
 
-    # json_file = open("sisco_format_fax.json", "r")
-    # json_dict = json.load(json_file, "utf-8")
-    # json_str = template_json_data(json_dict, ensure_ascii=False)
     print(len(json_str))
-    # post_form("sisco_format_fax_sjis.json", 'ocr.jpg')
-
-    # with open("sisco_format_fax.json") as fh:
-    #    json_dict = json.loads(fh.read(), "utf-8")
-    #    json_str = json.dumps(json_dict)
-    #    print(len(json_str))
     image_file = open('ocr.jpg', "rb")
 
     post_form(json_str, image_file)
