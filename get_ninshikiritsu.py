d8aa897c174cc346df9486074760b30908f032b4
Takeshi_Umezaki <Takeshi_Umezaki@sis2.saison.co.jp>
2017-11-16 20:09:08 +0900

    一列OK

--- get_ninshikiritsu.py
+++ get_ninshikiritsu.py
@@ -34,13 +34,33 @@ if __name__ == '__main__':
         df_evidence = pd.DataFrame(df_correct[column_name][0:200])
         column_r = column_name + "_R"
         print(column_r)
-        df_temp = pd.DataFrame(df_result[column_name][0:200])
-        df_temp.columns = [column_r]
+        print(type(df_result[column_name][0:200]))
+        print(len(df_evidence))
+        # l1 = list(pd.DataFrame(df_result[column_name][0:200].valus.flatten()))
+        # print(len(l1))
+        df_evidence[column_r] = df_result[column_name][0:200]
+        print(df_evidence.columns)
+        # df_temp = pd.DataFrame(df_result[column_name][0:200])
+        # df_temp.columns = [column_r]
 
-        df_evidence = pd.concat([df_evidence, df_temp], axis=1)
-
+        # df_evidence = pd.concat([df_evidence, df_temp], axis=1)
+        print(type(df_evidence))
+                
+        # 列追加。本当にこれしかない？
         column_d = column_name + "_D"
-        df_evidence[column_d] = df_evidence.apply(lambda x: difflib.SequenceMatcher(None, x[column_name], x[column_r]).ratio(), axis=1)
+        df_evidence[column_d] = [1] * len(df_evidence.index)
+        print(df_evidence.columns)
+        # df_evidence[column_d] = df_evidence.apply(lambda x: difflib.SequenceMatcher(None, x[column_name], x[column_r]).ratio(), axis=1)
+        i = 0
+        for index, row in df_evidence.iterrows():
+            str1 = str(row[column_name])
+            str2 = str(row[column_r])
+            print(type(str1))
+            diff_ratio = difflib.SequenceMatcher(None, str1, str2).ratio()
+            df_evidence[column_d][index] = diff_ratio
+            i += 1
+
         break
+        #df_evidence[column_d] = ratio
 
     df_evidence.to_csv(_EVIDENCE_FILE_NAME)    
